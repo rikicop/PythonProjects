bc8fe456ca6a5d16349d7b2717501c79a320d543
rikicop <ruperto1@protonmail.com>
2021-01-31 19:14:39 -0400

    mas regex

--- StaticFileCreator/regexStaticFiles.py
+++ StaticFileCreator/regexStaticFiles.py
@@ -7,10 +7,10 @@ import time
 #Agregar esto al principio {% static 'website/images
 # y esto al fina %} 
 
-with open('index.html', 'r') as file:
+with open('entrada.html', 'r') as file:
     x = file.read()
 
-tagstartImg = re.sub(r"images/", "{% static 'website/images/", x)
+tagstartImg = re.sub(r"img/", "{% static 'website/img/", x)
 
 #Reemplazar Final .jpg"
 tagendImgJpg = re.sub(r"\.jpg", ".jpg' %}", tagstartImg)
@@ -21,7 +21,7 @@ tagendImgSvg = re.sub(r"\.svg", ".svg' %}", tagendImgPng)
 
 time.sleep(2)
 #Css Principio
-tagstartCss = re.sub(r"css/", "{% static 'website/css/", tagendImgSvg)
+tagstartCss = re.sub(r"\"css/", "{% static 'website/css/", tagendImgSvg)
 #Css Final
 tagendCss = re.sub(r"\.css", ".css' %}", tagstartCss)
 
@@ -29,12 +29,20 @@ time.sleep(2)
 
 #Js Princ
 tagstartJs = re.sub(r"js/", "{% static 'website/js/", tagendCss)
-#Css Final
+#Js Final
 tagendJs = re.sub(r"\.js", ".js' %}", tagstartJs)
 print(tagendJs)
 
-filename = 'new-index.html'
+#lib
+tagstartLib = re.sub(r"lib/", "{% static 'website/lib/", tagendJs)
+
+print(tagstartLib)
+
+
+
+
+filename = 'salida.html'
 
 # Open the file in write mode and store the content in file_object
 with open(filename, 'w') as file_object:
-    file_object.write(tagendJs)
+    file_object.write(tagstartLib)
